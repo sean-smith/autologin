b702ce08d2831250c5276d4a12b1962cdcf8560a
Sean Smith <seanwssmith@gmail.com>
2017-02-08 12:46:51 -0500

    "Here's something to think about:  How come you never see a headline like `Psychic Wins Lottery'?" 		-- Jay Leno

--- js/popup.js
+++ js/popup.js
@@ -25,3 +25,14 @@ function get(field) {
 		$(`input[name='${field}']`).val(obj[field]);
 	});
 }
+
+// Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-91482366-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
