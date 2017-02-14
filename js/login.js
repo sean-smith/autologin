757e0da259533672b1c59120920931751f988a27
Nico Hinderling <nicolas.hinderling@gmail.com>
2017-02-13 23:14:49 -0500

    blackboard + gmail compatibility

--- js/login.js
+++ js/login.js
@@ -1,21 +1,37 @@
 
 $(function() {
-	// Login sequence
-	loading();
-	chrome.storage.sync.get("username", function(obj) {
-		console.log(obj["username"]);
-		$("#username").val(obj["username"]);
-	});
-	chrome.storage.sync.get("password", function(obj) {
-		$("#password").focus();
-		$("#password").val(obj["password"]);
-		$('[name="pw"]').val(obj["password"]);
-		$('[name="theform"]').submit();
-	});
+    // Login sequence [shib.bu.edu, shib2.bu.edu]
+    if (["shib.bu.edu", "shib2.bu.edu"].includes(window.location.hostname)) {
+        loading("form");
+        chrome.storage.sync.get("username", function(obj) {
+            document.getElementById('j_username').value = obj["username"];
+        });
+        chrome.storage.sync.get("password", function(obj) {
+            document.getElementById('j_password').value = obj["password"];
+            if (window.location.hostname === "shib2.bu.edu") {
+                $(".input-submit").click();
+            } else {
+                $("button").click();
+            }
+        });
+        return;
+    }
+    // Login sequence [weblogin.bu.edu]
+    loading('[name="theform"]');
+    chrome.storage.sync.get("username", function(obj) {
+        console.log(obj["username"]);
+        $("#username").val(obj["username"]);
+    });
+    chrome.storage.sync.get("password", function(obj) {
+        $("#password").focus();
+        $("#password").val(obj["password"]);
+        $('[name="pw"]').val(obj["password"]);
+        $('[name="theform"]').submit();
+    });
 });
 
-function loading() {
-	$('[name="theform"]').hide();
-	var path = chrome.extension.getURL('/img/loading.svg');
-	$('[name="theform"]').after(`<h2>Logging you in...</h2><div style='margin: auto;'><img src='${path}'></img></div>`);
+function loading(selector) {
+    $(selector).hide();
+    var path = chrome.extension.getURL('/img/loading.svg');
+    $(selector).after(`<h2>Logging you in...</h2><div style='margin: auto;'><img src='${path}'></img></div>`);
 }
